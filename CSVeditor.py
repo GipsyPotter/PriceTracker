04efa5232a4ddbf240b0b70ba8e0b84e1af7ae21
ThePotter <nguyentadac@gmail.com>
2022-12-22 10:15:31 +0700

    Testing CSV

--- CSVeditor.py
+++ CSVeditor.py
@@ -3,4 +3,8 @@ import time
 
 
 df = pd.read_csv('Product.csv', index_col='id')
+print(time.strftime("%H:%M:%S", time.localtime()))
+# if df.loc[1] == None
+# df.loc[1] = [time.strftime("%H:%M:%S", time.localtime()), ""]
 print(df)
+df.to_csv('Product.csv')
